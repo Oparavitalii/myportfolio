5d9bd6dc99ab1b3715d87fa29cd5a3868a6965b2
Vitalii Opara <oparavitalii@gmail.com>
2020-10-10 14:10:55 +0200

    dded responsive design

--- Oparaportfolio/js/script.js
+++ Oparaportfolio/js/script.js
@@ -11,14 +11,9 @@ $(document).ready(function() {
       return false;
    });
  
- 
-});
-$(document).ready(function() {
-	$('.header__burger').click(function(event) {
-		$('.header__burger').toggleClass('active');
-      $('.burger').toggleClass('activ');
-      $('.navburger').toggleClass('activpower');
-      $('.header__wrap').toggleClass('na')
-		
-	});
+ $(".burger__menu").click(function(){
+
+
+   $(".burger__menu").toggleClass("active");
+ })
 });
